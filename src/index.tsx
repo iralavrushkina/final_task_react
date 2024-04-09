4be7360f7d3cb9f99acebfc60b32e975061eb14e
iralavrushkina <77169100+iralavrushkina@users.noreply.github.com>
2024-04-09 11:57:05 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/index";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store/index'
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
-);
+	<React.StrictMode>
+		<Provider store={store}>
+			<HashRouter>
+				<App />
+			</HashRouter>
+		</Provider>
+	</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
