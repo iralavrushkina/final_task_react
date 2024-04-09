65d4060c4d8952f5f0e4371faa5e556d47cf1902
iralavrushkina <77169100+iralavrushkina@users.noreply.github.com>
2024-04-09 13:50:38 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/index'
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<HashRouter basename={process.env.PUBLIC_URL}>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
 				<App />
-			</HashRouter>
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 )
