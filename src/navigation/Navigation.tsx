d3fa594ac497809b4b40e3414edad151022473fb
iralavrushkina <77169100+iralavrushkina@users.noreply.github.com>
2024-04-09 11:31:30 +0300

    nav

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import PostList from "../components/PostList";
-import TodoList from "../components/TodoList";
-import UserList from "../components/UserList";
-import Layout from "../components/Layout";
-import UserInfo from "../components/UserInfo";
+import { Routes, Route } from 'react-router-dom'
+import PostList from '../components/PostList'
+import TodoList from '../components/TodoList'
+import UserList from '../components/UserList'
+import Layout from '../components/Layout'
+import UserInfo from '../components/UserInfo'
 
 const Navigation = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="postlist" element={<PostList />}></Route>
-                    <Route path="todoList" element={<TodoList />}></Route>
-                    <Route path="userList" element={<UserList />}></Route>
-                    <Route path="userList/:id" element={<UserInfo />}></Route>
-                </Route>
-            </Routes>
-        </>
-    );
-};
+	return (
+		<>
+			<Routes>
+				<Route path="/final_task_react/" element={<Layout />}>
+					<Route path="postlist" element={<PostList />}></Route>
+					<Route path="todoList" element={<TodoList />}></Route>
+					<Route path="userList" element={<UserList />}></Route>
+					<Route path="userList/:id" element={<UserInfo />}></Route>
+				</Route>
+			</Routes>
+		</>
+	)
+}
 
-export default Navigation;
+export default Navigation
